c3c2d85037ef297da4296a372f136c5d3907f550
HASHIR MUHAMMED S <iamhashir7@gmail.com>
2023-11-22 19:17:29 +0530

    0.002

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -1,12 +1,69 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { updateProfileAPI } from '../services/allApis'
 
 function Profile() {
+
+    const [userProfile,setUserProfile] = useState({
+        _id:JSON.parse(localStorage.getItem("existingUser"))._id,
+        username:JSON.parse(localStorage.getItem("existingUser")).username,
+        email:JSON.parse(localStorage.getItem("existingUser")).email,
+        password:JSON.parse(localStorage.getItem("existingUser")).password,
+        github:"",
+        linkedin:"",
+        profileImage:""
+    })
+
+    const [preview,setPreview] = useState("")
+
+    useEffect(()=>{
+        if(userProfile.profileImage){
+            setPreview(URL.createObjectURL(useProfile.profileImage))
+        }else{
+            setPreview("")
+        }
+    },[useProfile.profileImage])
+
+    const handleUpdate = async (e) => {
+        e.preventDefault()
+        const {_id,username,email,password,github,linkedin,profileImage} = userProfile
+        if(!_id || !username || !email || !password || !github || !linkedin){
+            alert("Please fill the form completely")
+        }else{
+            const reqBody = new FormData()
+            reqBody.append("username",username)
+            reqBody.append("email",email)
+            reqBody.append("password",password)
+            reqBody.append("github",github)
+            reqBody.append("linkedin",linkedin)
+            reqBody.append("profileImage",profileImage)
+            const token = sessionStorage.getItem("token")
+            if(preview){
+                const reqHeader = {
+                    "Content-Type":"multipart/form-data","Authorization":`Bearer ${token}`
+                }
+                const result = await updateProfileAPI(reqBody,reqHeader)
+                if(result.status === 200){
+                    alert("profile updated")
+                    setUserProfile({
+                        _id:result.data._id,username:result.data.username,email:result.data.email,password:result.data.password,github:result.data.github,linkedin:result.data.linkedin,profileImage:""
+                    })
+                }else{
+                    alert(result)
+                }
+            }else{
+                const reqHeader = {
+                    "Content-Type":"application/json","Authorization":`Bearer ${token}`
+                }
+            }
+        }
+    }
+
   return (
     <>
         <div className='card shadow p-5'>
             <div className="d-flex justify-content-between">
                 <h2>My Profile</h2>
-                <button className='btn btn-outline-primary'><i className='fa-solid fa-check'></i></button>
+                <button onClick={handleUpdate} className='btn btn-outline-primary'><i className='fa-solid fa-check'></i></button>
             </div>
             <div className='row justify-content-center mt-3'>
                 {/* profile picture */}
@@ -18,7 +75,7 @@ function Profile() {
                     <input type="text"  className='form-control' placeholder='Username'/>
                 </div>
                 <div className='mb-3'>
-                    <input type="text"  className='form-control' placeholder='Github Profile Link'/>
+                    <input type="text" onChange={e=>setUserProfile} value={useProfile.github}  className='form-control' placeholder='Github Profile Link'/>
                 </div>
                 <div className='mb-3'>
                     <input type="text"  className='form-control' placeholder='LinkedIn Profile Link'/>
